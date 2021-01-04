c03ad7092c4228cc3599794ae2e55c95a5852e1a
taiwo2 <adetonat@ymail.com>
2021-01-04 14:42:57 +0100

    Add  main.rb content

--- bin/main.rb
+++ bin/main.rb
@@ -19,7 +19,7 @@ and we will focus on the 3 x 3 grid here.
 #step 3
 Have the first player go first. Though traditionally, the
  first player goes with "X", you can allow the first player to 
- decide whether he wants to go with "X"s or "O"s. These symbols will be
+ decide whether he wants to go with "X"s or 'O's. These symbols will be
   placed on the table, in the attempt to have three of them in a row. 
   If you are going first, then the best move you can make is to move into 
   the center. This will maximize your chances of winning, since you will 
@@ -45,9 +45,67 @@ different from the symbol of the first player. The second player can
    However, if both players are playing with optimal strategy, then there is a good 
    chance that no one will win because you will have blocked one another opportunities to create a row of three
    # puts 'Draw'
+
  step6
  Keep practicing. Contrary to popular belief, tic-tac-toe is not purely a game of chance.
   There are some strategies that can help you optimize your skills and to become an expert 
   tic-tac-toe player. If you keep playing, you will soon learn all of the tricks to making 
   sure you win every time -- or, at least, you will learn the tricks to make sure you 
  never lose. Its like 0's and x's.
+
+ 
+ Directory structure
+We'll be building 3 classes to construct our basic game.
+ Begin by creating a tic_tac_toe_v1 
+directory with the following structure:
+
+Board
+The Board class is responsible for adding a player's marks, checking for winners, and 
+printing the game board. A board instance must have an attribute to represent the grid. 
+For simplicity, give the grid the dimensions of classic tic-tac-toe, 3x3.
+
+Board#initialize
+A Board instance must have an instance variable to represent the game grid. For now, 
+Board#initialize does not need to accept any arguments. Initialize the grid so 
+that all positions are empty; this means that every position 
+should contain an underscore (1..9).
+
+Board#win?(mark)
+This method should return a boolean indicating whether or not the
+ given mark has filled any full row, column, or diagonal.
+
+ main.rb
+The Game class will be the main class that houses the instances of Board and Player
+ required for gameplay. 
+It will be responsible for passing data between the board and players. 
+It will also serve to contain the main game loop.
+
+Be sure to require your board.rb and player.rb files into game.rb.
+
+Game#initialize
+An instance of Game should have instance variables for player one, player two,
+ and the board. Design the
+ #initialize method to accept the mark values to be used for the players.
+
+You should also initialize an instance variable to contain the current player. 
+By default, player one should begin as the current 
+
+ Game#play
+This method will contain the main game loop. Here is some psuedocode for the loop:
+    
+while there exists empty positions on the board
+ print the board
+    get a position from the current player
+    place their mark at that position of the board
+    check if that user has won
+    if they win, print out a 'victory' message saying who won and return to end the game
+    otherwise they did not win, so switch turns
+    if we finish the while loop without returning, that means there are no more empty positions on the board and
+     none has won, so print a 'draw' message
+    Test your game in pry. Play a few games by creating an instance of Game and invoking #play on it. 
+    After a game is over, be sure to create a new instance of Game to play again,
+     this is the only way to get a fresh board. Some scenarios to test are: player one winning, 
+     
+     player two winning, and a draw.
+    
+    If any errors are raised during gameplay, your game loop and #play method will end immediately, so you'll have to start a new game and retry!
