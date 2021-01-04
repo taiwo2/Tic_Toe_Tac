63c804dea75b1dd8e41c00c8c18c73e2e9f6c3d6
taiwo2 <adetonat@ymail.com>
2021-01-04 14:15:47 +0100

    delete folder

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,53 @@
 #!/usr/bin/env ruby
 puts 'Hello World'
+
+     TIC_TOE_TAC
+Tic-tac-toe is a fun game that you can play any time 
+and anywhere as long as you have a piece of paper, a pencil,
+ and an opponent. Tic-tac-toe is a zero sum game, which means that
+ 
+  if both players are playing their best, that neither player will win.
+   However, if you learn how to play tic-tac-toe and master some simple
+    strategies, then you will be able to not only play, but to win the majority of
+     the time. If you want to know how to play tic-tac-toe, then see Step 1 to get started.
+     
+   #step 1
+Draw the board. First, you have to draw the board, which is 
+made up of a 3 x 3 grid of squares. This means it has three rows of
+ three squares. Some people play with a 4 x 4 grid, but that is for more advanced players, 
+and we will focus on the 3 x 3 grid here.
+#step 3
+Have the first player go first. Though traditionally, the
+ first player goes with "X", you can allow the first player to 
+ decide whether he wants to go with "X"s or "O"s. These symbols will be
+  placed on the table, in the attempt to have three of them in a row. 
+  If you are going first, then the best move you can make is to move into 
+  the center. This will maximize your chances of winning, since you will 
+  be able to create a row of three "X"s or "O"s in more combinations (4) this 
+way than if you chose a different square.
+#step 4
+Have the second player go second. After the first player goes, 
+then the second player should put down his symbol, which will be 
+different from the symbol of the first player. The second player can
+ either try to block the first player from creating a row of three, 
+ or focus on creating his or her own row of three.
+ Ideally, the player can do both.
+
+ step5
+ Keep alternating moves until one of the players has 
+ drawn a row of three symbols or until no one can win. 
+ The first player to draw three of his or her symbols in a row,
+  whether it is horizontal, vertical, or diagonal, has won tic-tac-toe.
+   will be declare  and prompted as
+   # puts 'you win on the board'
+   otherwise
+   puts  'you lose on the board for other player'
+   However, if both players are playing with optimal strategy, then there is a good 
+   chance that no one will win because you will have blocked one another opportunities to create a row of three
+   # puts 'Draw'
+ step6
+ Keep practicing. Contrary to popular belief, tic-tac-toe is not purely a game of chance.
+  There are some strategies that can help you optimize your skills and to become an expert 
+  tic-tac-toe player. If you keep playing, you will soon learn all of the tricks to making 
+  sure you win every time -- or, at least, you will learn the tricks to make sure you 
+ never lose. Its like 0's and x's.
