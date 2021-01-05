90a539014a61b2d05c7da1e35668779c9353d109
taiwo2 <adetonat@ymail.com>
2021-01-05 13:19:22 +0100

    add spec

---
+++ rspec/game_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require_relative '../lib/game'
+
+describe Game do
+  let(:new_game) { Game.new }
+  let(:board) { Board.new }
+  let(:player1) { Player.new('David', 'X') }
+  let(:player2) { Player.new('Oyinda', 'O') }
+  let(:active_player) { player1 }
+
+  describe '#validate_name' do
+    it 'checks if a name has been entered' do
+      expect(new_game.validate_name('David')).to eql('David')
+    end
+  end
+
+  describe '#validate_position' do
+    it 'checks if the position taken is valid' do
+      expect(new_game.validate_position(3, board.board)).to eql(3)
+    end
+
+    it 'returns false if the position taken is not valid' do
+      expect(new_game.validate_position(11, board.board)).to_not eql(true)
+    end
+  end
+
+  describe '#game_status' do
+    it 'returns false if the game is neither a win nor a draw ' do
+      expect(new_game.game_status?(board, active_player)).to eql(false)
+    end
+
+    it 'returns true if the game is neither a win nor a draw ' do
+      expect(new_game.game_status?(board, active_player)).to_not eql(true)
+    end
+  end
+end
